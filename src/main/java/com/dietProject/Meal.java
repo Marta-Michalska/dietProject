32ab2e1dd7a5ac9c522adc3f841405beddac17a2
Tusiak <konecka.marta@gmail.com>
2020-07-06 22:57:26 +0200

    small fixes

--- src/main/java/com/dietProject/Meal.java
+++ src/main/java/com/dietProject/Meal.java
@@ -1,13 +1,9 @@
 package com.dietProject;
 
-import javafx.beans.binding.MapExpression;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Data
 @AllArgsConstructor
